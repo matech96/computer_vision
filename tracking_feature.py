bdcdf580bd7282ee6538aab6c3d3693f5302ef68
matech <matech@sch.bme.hu>
2018-04-25 15:05:16 +0200

    optical flow startup

--- tracking_feature.py
+++ tracking_feature.py
@@ -7,7 +7,7 @@ import matech_utilities as mu
 plt.rcParams['figure.figsize'] = (20, 15)
 
 img_dir = "images/"
-MIN_MATCH_COUNT = 10
+MIN_MATCH_COUNT = 8
 # r, h, c, w = 450, 100, 890, 100  # rubber
 # r,h,c,w = 250,300,850,75 # election day bracelet
 # r,h,c,w = 200,200,800,350 # blue
@@ -22,55 +22,46 @@ org_box = np.array([[c, r, 1],
                     ])
 
 cap = cv.VideoCapture(img_dir + "slow_calculator.mp4")
-_, first_frame = cap.read()
-org_mask = mu.create_mask(first_frame)
+_, org_frame = cap.read()
+org_frame = cv.cvtColor(org_frame, cv.COLOR_BGR2GRAY)
+org_mask = mu.create_mask(org_frame)
 org_mask[r:r + h, c:c + w] = 255
-_, org_pts, org_feat = mu.extract_features(first_frame, resize=False, mask=org_mask)
-# mu.cv_show_pics(1, 1, [mu.draw_points([m.pt for m in org_pts], first_frame)])
-prev_center = np.mean(org_box[:, :-1], axis=0)
+_, org_pts, org_feat = mu.extract_features(org_frame, resize=False, mask=org_mask)
+org_pts = np.array([p.pt for p in org_pts], dtype=np.float32).reshape((-1, 1, 2))
+mu.show_pics(1, 1, [mu.draw_points(org_pts.reshape((-1, 2)), org_frame)])
+
 prev_pts = org_pts
 prev_feat = org_feat
 prev_box = org_box
+prev_frame = org_frame
 
 fourcc = cv.VideoWriter_fourcc(*'DIVX')
-vh, vw, _ = first_frame.shape
-out = cv.VideoWriter('output.mkv', fourcc, 25.0, (vw, vh), isColor=True)
+vh, vw = org_frame.shape
+out = cv.VideoWriter('output2.mkv', fourcc, 25.0, (vw, vh), isColor=True)
+lk_params = dict(winSize=(15, 15),
+                 maxLevel=2,
+                 criteria=(cv.TERM_CRITERIA_EPS | cv.TERM_CRITERIA_COUNT, 10, 0.03))
 
 while True:
     ret, frame = cap.read()
 
     if ret:
-        # print(type(prev_feat), type(prev_pts))
-        # print(prev_feat.shape, len(prev_pts))
-        _, pts, feat = mu.extract_features(frame, resize=False)
-        matches = mu.match_features(prev_feat, feat)
-        if len(matches) > MIN_MATCH_COUNT:
-            m_prev_pts, m_pts = mu.match_points(matches, prev_pts, pts)
-            h_m_prev_pts = np.float32([m.pt for m in m_prev_pts]).reshape(-1, 1, 2)
-            h_m_pts = np.float32([m.pt for m in m_pts]).reshape(-1, 1, 2)
-            h, _ = cv.findHomography(h_m_prev_pts, h_m_pts, cv.RANSAC, 5.0)
-            box = mu.transform_with_homography(h, prev_box)
-            # mask[:, :] = 0
-            #             ptsa = cs.reshape((-1,1,2))
-            # mask = cv.fillPoly(mask, [cs], 1)
-            # org_feat = feat
-            # org_pts = pts
-            res, center = mu.draw_box_homogeneous(box, frame, True)
-            res = mu.draw_points(np.array([m.pt for m in m_prev_pts]), res)
-            prev_pts = m_pts
-            prev_feat = np.array([feat[m.trainIdx] for m in matches])
-            prev_box = box
-            # speed = center - prev_center
-            #             cv_show_pics(frame)
-            #             print(cs)
-            # mu.cv_show_pics(1, 1, [res])
-            out.write(res)
-            # prev_center = center
-        else:
-            print("Not enough matches are found - {}/{}".format(len(matches), MIN_MATCH_COUNT))
-
+        gray_frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+        pts, st, err = cv.calcOpticalFlowPyrLK(org_frame, prev_frame, prev_pts, None, **lk_params)
+        m_pts = pts[st == 1]
+        m_prev_pts = prev_pts[st == 1]
+        # h, _ = cv.findHomography(m_prev_pts, m_pts, cv.RANSAC, 5.0)
+        # box = mu.transform_with_homography(h, prev_box)
+        # res, center = mu.draw_box_homogeneous(box, frame, True)
+        res = frame
+        res = mu.draw_points(m_pts, res)
+        prev_pts = m_pts.reshape(-1, 1, 2)
+        # prev_box = box
+        prev_frame = gray_frame
+        # mu.cv_show_pics(1, 1, [res])
+        out.write(res)
     else:
         break
-out.release()
 
+out.release()
 cap.release()
